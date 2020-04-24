225785f1505ebcc143ccb345962217a4496d8dc7
Mukul Kansal <mukul.kansal@jungleworks.com>
2020-04-24 20:48:10 +0530

    framework added

--- LocationTracker/build/LocationTracker.build/Release-iphoneos/LocationTracker.build/DerivedSources/LocationTracker_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char LocationTrackerVersionString[];
- extern const double LocationTrackerVersionNumber;
-
- const unsigned char LocationTrackerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LocationTracker  PROJECT:LocationTracker-1" "\n";
- const double LocationTrackerVersionNumber __attribute__ ((used)) = (double)1.;
