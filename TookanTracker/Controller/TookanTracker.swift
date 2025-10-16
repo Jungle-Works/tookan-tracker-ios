7f189515b2ac28e193eac52680c3c8848cc9c8d8
Karan <>
2025-10-16 12:29:32 +0530

    delaytimer updated

--- TookanTracker/Controller/TookanTracker.swift
+++ TookanTracker/Controller/TookanTracker.swift
@@ -31,7 +31,7 @@ public class TookanTracker: NSObject, CLLocationManagerDelegate {
     var jobArray = [Jobs]()
     public var getETA = ""
     public var apiKey = ""
-    public var delayTimer = 60.0
+    public var delayTimer = 1.0
     public var googleMapKey = ""
     public var jobArrayCount = 0
     var locationManager:CLLocationManager!
